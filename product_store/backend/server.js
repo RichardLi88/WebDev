8573f5dee51b62a0d93d429e8357c8fca02d0c14
Richard Li <richard.li2004@hotmail.com>
2025-01-30 23:55:44 +1100

    create product-store

---
+++ product_store/backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server is ready");
+});
+
+app.listen(5000, () => {
+  console.log("Server started at http://localhost:5000");
+});
+
+//4UA0T6hQMiWOTelu
