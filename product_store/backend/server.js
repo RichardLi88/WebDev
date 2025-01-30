73fac2042792121acc61938816827e28b3696b87
Richard Li <richard.li2004@hotmail.com>
2025-01-31 00:37:18 +1100

    create product schema for database and connect to mongoosedb

--- product_store/backend/server.js
+++ product_store/backend/server.js
@@ -1,12 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
+import ConnectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("server is ready");
+  res.send("API is running...");
 });
 
 app.listen(5000, () => {
+  ConnectDB();
   console.log("Server started at http://localhost:5000");
 });
 
