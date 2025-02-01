0dfdfecdc8b318f19b01a2a0af6676dbbfa6fc51
Richard Li <richard.li2004@hotmail.com>
2025-02-02 02:29:06 +1100

    create getProduct function and render into homepage

--- product_store/frontend/src/pages/Home.jsx
+++ product_store/frontend/src/pages/Home.jsx
@@ -3,20 +3,23 @@ import Modal from "../components/Modal";
 import "../css/Home.css";
 import { modalContext } from "../context/modalContext";
 import Card from "../components/Card";
-import Pikachu from "../img/2.svg";
+
+import { getProducts } from "../function/functions";
 
 function Home() {
   const { modal, toggleModal } = useContext(modalContext);
   const [products, setProducts] = useState([]);
+
   useEffect(() => {
-    setProducts([
-      { name: "Hello", price: 100, image: Pikachu },
-      { name: "Hello", price: 100, image: Pikachu },
-      { name: "Hello", price: 100, image: Pikachu },
-      { name: "Hello", price: 100, image: Pikachu },
-      { name: "Hello", price: 100, image: Pikachu },
-      { name: "Hello", price: 100, image: Pikachu },
-    ]);
+    const getData = async () => {
+      try {
+        const { success, data } = await getProducts();
+        setProducts(data);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+    getData();
   }, []);
 
   return (
