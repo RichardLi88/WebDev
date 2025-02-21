be24db1f2c578f930d488227b101075d2ce90dc4
Richard Li <richard.li2004@hotmail.com>
2025-02-21 23:35:58 +1100

    Initial creation of Java Spring
    - project used to practice and learn java springboot

---
+++ Java/quickstart/src/main/java/dev/richard/quickstart/HelloWorldController.java
@@ -0,0 +1,12 @@
+package dev.richard.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+  @GetMapping(path = "/hello")
+  public String helloWorld() {
+    return "Hello Richard";
+  }
+}
